5fc9fbe25cec2ccbe0a491a1d3de74aa4ae7296b
Jasonq <jason@hiregen-ai.app>
2025-12-12 13:59:20 +0000

    feat: Update favicon to use umukozi-logo.png

--- components/SEOHead.tsx
+++ components/SEOHead.tsx
@@ -113,10 +113,9 @@ export default function SEOHead({
       />
 
       {/* Favicons */}
-      <link rel="icon" type="image/png" sizes="32x32" href="/media/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/media/favicon-16x16.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/media/apple-touch-icon.png" />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="icon" type="image/png" href="/media/umukozi-logo.png" />
+      <link rel="apple-touch-icon" href="/media/umukozi-logo.png" />
+      <link rel="shortcut icon" href="/media/umukozi-logo.png" />
     </Head>
   );
 }
